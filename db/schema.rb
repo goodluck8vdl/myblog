ec4493ee9104cb403be1ccf771f8b67291dac6df
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-02-20 22:47:03 +0000

    add image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216031303) do
+ActiveRecord::Schema.define(version: 20170220211220) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170216031303) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
 end
